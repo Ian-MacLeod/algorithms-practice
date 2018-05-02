93d92680933f1b1a5200aea6d53bb5e2fcec1540
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-01 18:41:11 -0700

    Solve palindrome problem using custom hash map

--- lru_cache/lib/p07_practical.rb
+++ lru_cache/lib/p07_practical.rb
@@ -1,5 +1,15 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  counts = HashMap.new
+  string.each_char do |chr|
+    if counts.include?(chr)
+      counts[chr] += 1
+    else
+      counts[chr] = 1
+    end
+  end
+  total = 0
+  counts.each { |x| total += 1 if x[1] % 2 == 1 }
+  total <= 1
 end
