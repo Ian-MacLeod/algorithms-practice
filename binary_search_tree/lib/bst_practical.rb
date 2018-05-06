f8f2de646b43a3b009586b6576e1b764884ae454
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-05 18:37:18 -0700

    Solve bst practical problem

--- binary_search_tree/lib/bst_practical.rb
+++ binary_search_tree/lib/bst_practical.rb
@@ -1,3 +1,17 @@
 
 def kth_largest(tree_node, k)
+  count = 1
+  path = [["pre", tree_node]]
+  until path.empty?
+    type, current = path.pop
+    if type == "in"
+      return current if count == k
+      count += 1
+    else
+      path << ["pre", current.left] unless current.left.nil?
+      path << ["in", current]
+      path << ["pre", current.right] unless current.right.nil?
+    end
+  end
+  nil
 end
